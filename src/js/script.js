c3d944baf093f518933c3fe3b7009bcf77232a0a
jankamienski <kamienski.jan@gmail.com>
2020-11-09 14:53:31 +0100

    During point 9.4

--- src/js/script.js
+++ src/js/script.js
@@ -398,7 +398,38 @@
 
       thisCart.dom.productList.appendChild(generatedDOM);
  
+      thisCart.products.push(menuProduct);
+      console.log('thisCart.products', thisCart.products);
 
+    }
+  }
+
+  class CartProduct{
+    constructor(menuProduct, element){
+      const thisCartProduct = this;
+
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amountWidgetElem = menuProduct.amount;
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+      thisCartProduct.getElements(element);
+
+      console.log('thisCartProduct'. thisCartProduct);
+      console.log('productData'. menuProduct);
+    }
+
+    getElements(element){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.wrapper.querySelector(select.cartProduct.remove);
     }
   }
 
